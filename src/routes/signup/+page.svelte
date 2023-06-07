6e9d7c37003f12dfb819025c0c013744efa39f6a
strong6208 <hamilton6208@gmail.com>
2023-06-06 18:26:17 -0700

    [feat] build ui on the register page

---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	import { faWarning } from "@fortawesome/free-solid-svg-icons";
+	import Fa from "svelte-fa";
+	import type { ActionData } from "./$types";
+	import debug from "debug";
+	import { goto } from "$app/navigation";
+
+	const log = debug("app:routes:signup:page.svelte");
+
+	export let form: ActionData;
+</script>
+
+<section class="max-w-sm mx-auto mt-56">
+	<div class="prose">
+		<h1>Sign Up</h1>
+	</div>
+	<div class="flex gap-x-6 mt-4">
+		<h1>Have Account ? </h1>
+		<p>
+			<a href="/login" class="link">Log In</a>
+		</p>
+	</div>
+
+	<form
+		class="flex flex-col gap-6 my-6"
+		method="POST"
+	>
+		{#if form?.error}
+			<div class="alert alert-error">
+				<div>
+					<Fa icon={faWarning} />
+					{form.error}
+				</div>
+			</div>
+		{/if}
+		<p>
+			<input
+				type="email"
+				name="email"
+				placeholder="Email..."
+				class="input input-bordered w-full"
+				required
+				value={form?.email ?? ""}
+			/>
+		</p>
+		<p>
+			<input
+				type="password"
+				name="password"
+				placeholder="Password..."
+				class="input input-bordered w-full"
+				required
+			/>
+		</p>
+		<p>
+			<input
+				type="password"
+				name="password-confirm"
+				placeholder="Confirm password..."
+				class="input input-bordered w-full"
+				required
+			/>
+		</p>
+		<p class="flex items-center gap-6 mt-6">
+			<button class="btn btn-primary w-full">Sign Up</button>
+		</p>
+	</form>
+
+	{#if form}
+		<section class="my-8">
+			<h3>Form data:</h3>
+			<pre>{JSON.stringify(form, null, 2)}</pre>
+		</section>
+	{/if}
+</section>
