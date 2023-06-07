827759e036b62b60771b0bbdfd77fd7470fd19c1
strong6208 <hamilton6208@gmail.com>
2023-06-07 02:14:35 -0700

    [feat] create contact component

---
+++ src/routes/chats/Contact.svelte
@@ -0,0 +1,15 @@
+<script lang="ts"></script>
+<div>
+    <img
+        alt="current user profile"
+        class="w-10 aspect-square"
+    />
+    <!-- {active && (
+        <div class="w-4 aspect-square rounded-full bg-green-500 absolute right-0 bottom-0 border-2 border-cc-200 dark:border-neutral-800"></div>
+    )} -->
+    <div class="hidden lg:block leading-relaxed">
+        <!-- <h1>{user?.username}</h1>
+        <p>{user?.email}</p> -->
+    </div>
+</div>
+  
