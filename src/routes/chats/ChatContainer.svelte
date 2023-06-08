c0614f1820cd3b14fc96fbb799641924ac3726ca
strong6208 <hamilton6208@gmail.com>
2023-06-08 05:25:45 -0700

    [upd] chat container

--- src/routes/chats/ChatContainer.svelte
+++ src/routes/chats/ChatContainer.svelte
@@ -1,43 +1,97 @@
 <script lang="ts">
-    import Contact from "./Contact.svelte"
-    import EmojiMultiple from "../../components/EmojiMultiple.svelte"
-    import Send from "../../components/Send.svelte"
+  import { onMount } from "svelte"
+  import axios from "axios"
+
+  import Contact from "./Contact.svelte"
+  import EmojiMultiple from "../../components/EmojiMultiple.svelte"
+  import Send from "../../components/Send.svelte"
+
+  export let currentUser:User
+  export let currentChat:Chat
+
+  let messages: any[] = [
+    {
+      fromSelf: "abcd",
+      message: "Hello, how are you?"
+    },
+    {
+      fromSelf: "ABCD",
+      message: "Hello, Nice to meet you!"
+    }
+  ]
+
+  let formData = {
+    messageToSend: "aa"
+  }
+
+  const handleFormSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+
+    await axios.post("api/sendMessage", {
+      from: currentUser?.id,
+      to: currentChat?.id,
+      message: formData.messageToSend,
+    });
+
+    const msgs = [...messages];
+    msgs.push({ fromSelf: true, message: formData.messageToSend });
+    messages = msgs;
+    formData.messageToSend = "";
+  };
+  
+  /* get chat history from the backend */
+  onMount(async () => {
+    const getMessages = async () => {
+      const response = await fetch(`api/getMessages`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          from: currentUser.id,
+          to: currentChat.id
+        })
+      });
+      const data = await response.json();
+      messages = data;
+    };
+
+    await getMessages();
+  });
 </script>
 
 <section class="static flex flex-col h-full text-cc-400 dark:text-white">
   <!-- header -->
   <div class="py-4 leading-relaxed text-sky-700 text-center border-solid border-grey-100">
-      John Doe
-      <!-- <h1>{user?.username}</h1>-->
+      <h1>{currentUser?.userName}</h1>
   </div>
   <!-- chat screen -->
-  <div class="p-8 flex-1 overflow-y-scroll space-y-2 bg-zinc-200">
-    <!--{messages.length > 0 ? (
-    //   messages.map((message) => {
-    //     return (
-    //       <div
-    //         ref={scrollRef}
-    //         key={uuidv4()}
-    //         class={`flex ${
-    //           message.fromSelf ? `justify-end` : `justify-start`
-    //         }`}>
-    //         <div class="flex flex-col items-end leading-relaxed space-y-2">
-    //           <div
-    //             class={`max-w-4xl px-4 py-2 rounded-sm dark:text-white ${
-    //               message.fromSelf
-    //                 ? `bg-cc-200 dark:bg-neutral-800`
-    //                 : `bg-cc-400 text-cc-100 dark:bg-violet-600 `
-    //             }`}>
-    //             <h2>{message.message}</h2>
-    //           </div>
-    //           <span class="text-xs text-cc-400 dark:text-gray-400">
-    //             {format(message.createdAt)}
-    //           </span>
-    //         </div>
-    //       </div>
-    //     );
-    //   })
-    // ) : (  -->
+  <div class="p-8 flex-1 h-auto overflow-y-scroll space-y-2 bg-zinc-200">
+    {#if messages?.length > 0}
+      {#each messages as message}
+        <div
+          class={`flex ${
+            message.fromSelf ? `justify-end` : `justify-start`
+          }`}
+        >
+          <div class="flex flex-col items-end leading-relaxed space-y-2">
+            <div
+              class={`max-w-4xl px-4 py-2 rounded-sm dark:text-white ${
+                message.fromSelf
+                  ? `bg-cc-200 dark:bg-neutral-800`
+                  : `bg-cc-400 text-cc-100 dark:bg-violet-600 `
+              }`}>
+              <h2>{message.message}</h2>
+            </div>
+            <span class="text-xs text-cc-400 dark:text-gray-400">
+              <!-- {format(message.createdAt)}
+               ref={scrollRef}
+                key={uuidv4()} -->
+            </span>
+          </div>
+        </div>
+      {/each}
+    {:else}
       <div class="grid place-content-center h-[38rem] text-center space-y-2 px-8">
         <h1> 
             <!-- {currentUser?.username},  -->
@@ -48,23 +102,26 @@
             <!-- {currentChat?.username} -->
         </p>
       </div>
-      <!-- chat input -->
-      <form
-        class="absolute bottom-0 rounded-[20px] bg-white px-3 py-4 flex items-center sticky"
-      >
-        <div class="icon-style">
-          <EmojiMultiple />
-        </div>
-        <textarea
-          placeholder="type you message here..."
-          class="input input-bordered bg-transparent w-full mx-2 text-cc-400 dark:text-white"
-        />
-        <!-- {messageToSend.trim() && ( -->
-        <button class="icon-style">
-          <Send />
-        </button>
-        <!-- )} -->
-      </form>
+    {/if}
+
+    <!-- chat input -->
+    <form
+      on:submit={handleFormSubmit}
+      class="absolute bottom-0 rounded-[20px] bg-white px-3 py-4 flex items-center sticky"
+    >
+      <div class="icon-style">
+        <EmojiMultiple />
+      </div>
+      <textarea
+        name="message"
+        bind:value={formData.messageToSend}
+        placeholder="type you message here..."
+        class="input input-bordered bg-transparent w-full mx-2 text-cc-400 dark:text-white"
+      />
+      <button class="icon-style">
+        <Send />
+      </button>
+    </form>
   </div>
   
 </section>
