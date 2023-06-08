1cf5476174f12bd2256fbe9840ba780379a4136f
strong6208 <hamilton6208@gmail.com>
2023-06-07 18:52:04 -0700

    [fix] tab style

--- src/routes/chats/Contacts.svelte
+++ src/routes/chats/Contacts.svelte
@@ -1,38 +1,23 @@
 <script lang="ts">
     import SignOut from './SignOut.svelte';
     import Contact from './Contact.svelte';
-    import Theme from '../../components/Theme.svelte';
 </script>
   
   <section class="bg-slate-100 shadow shadow-slate-500/40 text-cc-400 flex flex-col col-span-1 lg:col-span-2 xl:col-span-1 h-screen">
-    <div class="shadow shadow-blue-500/40">
-      <ul
-        class="flex list-none flex-row flex-wrap border-b-0 pl-0"
-        role="tablist"
-      >
-        <li role="presentation">
-          <a
-            href="#tabs-home0"
-            class="block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
-            data-te-toggle="pill"
-            data-te-target="#tabs-home0"
-            data-te-nav-active
-            role="tab"
-            aria-controls="tabs-home0"
-            aria-selected="true"
-          >CHATS</a>
-        </li>
-        <li role="presentation">
-          <a
-            href="#tabs-profile0"
-            class="focus:border-transparen block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
-            data-te-toggle="pill"
-            data-te-target="#tabs-profile0"
-            role="tab"
-            aria-controls="tabs-profile0"
-            aria-selected="false"
-          >USERS</a>
-        </li>
+    <div class="border-b border-gray-200 dark:border-gray-700">
+      <ul class="hidden text-sm font-medium text-center text-gray-500 rounded-lg shadow sm:flex dark:divide-gray-700 dark:text-gray-400">
+          <li class="w-full">
+              <a href="#" class="inline-flex p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-sky-300 group">
+                <svg aria-hidden="true" class="w-5 h-5 mr-2 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path><path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd"></path></svg>  
+                  CHATS
+              </a>
+          </li>
+          <li class="w-full">
+              <a href="#" class="inline-flex p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-sky-300 group">
+                <svg aria-hidden="true" class="w-5 h-5 mr-2 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
+                  USERS
+              </a>
+          </li>
       </ul>
     </div>
     <div class="flex items-center justify-center lg:justify-start space-x-4 hover:bg-cc-100 dark:hover:bg-neutral-900 cursor-pointer lg:px-8 py-4">
